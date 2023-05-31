8383f128400acfca6b75cdd30c681bba9193f33d
KirahhY <marius.oudin@gmail.com>
2023-05-31 10:21:00 +0200

    what

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                     <iframe src="https://discord.com/widget?id=1087078666850287766&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
                 </div>
             </div>
-
+test
             <div class="game">
                 <button class="play-btn">PLAY</button>
             </div>
