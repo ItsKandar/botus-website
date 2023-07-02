d495692ea3b76a28d91d02bb640becfab7fbe3de
Luma168 <ademben25@outlook.com>
2023-07-02 14:04:03 +0200

    update js

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,14 @@
         // Mot à deviner
         var targetWord = "";
 
+        // Mot deviné
+        var userInput = "";
+
         // Nombre de tentatives
         var attempts = 0;
+
+        // Le résultat
+        var result = "";
 
         // Fonction pour choisir un mot aléatoire
         function chooseWord() {
@@ -18,48 +24,55 @@
             // document.getElementById("word-input").value = "";
             // document.getElementById("result").innerHTML = "";
             printWord(targetWord);
-            attempts = 0;
-            document.getElementById("attempt-count").innerHTML += attempts;
+            $("#attempt-count").children().text(attempts);
         }
 
         // Fonction pour afficher un mot
         async function printWord(targetWord) {
+            $("#attempt-count").children().text(attempts);
+
+            var row = document.createElement("div");
+            row.classList.add("row");
+            $(row).appendTo("#box").hide().fadeIn(500);
             for ( let i = 0; i < targetWord.length; i++ ) {  
-                printLetter(targetWord[i], i);
+                printLetter(targetWord[i], i, row);
                 await sleep(100);
             }
         }
 
         // Fonction pour afficher une lettre
-        function printLetter(letter, index) {
+        function printLetter(letter, index, row) {
             input = document.createElement("input");
             input.classList.add("letter");
             input.setAttribute("maxlength", 1);
-            $(input).appendTo("#box").hide().fadeIn(500); 
+            $(input).appendTo(row).hide().fadeIn(500); 
             if ( index == 0 ) {
                 input.value = letter
             }
             $(input).on( "change", function() {
-                checkInputs();
+                var lastRow = $('.row:last');
+                checkInputs(row);
             });
         }
 
         // Fonction pour vérifier si tous les inputs sont remplis
-        function checkInputs(){
+        function checkInputs(row){
             var filled = 0;
-            $('.letter').each(function(){           
+            $(row).children().each(function(){           
                 if ( $(this).val() ) {
                     filled ++;
                 } 
             });
             if (filled == targetWord.length) {
-                checkWord();
+                checkWord(row);
             }
         }
         // Fonction pour vérifier le mot entré par l'utilisateur
-        function checkWord() {
+        function checkWord(row) {
             var index = 0;
-            $('.letter').each(function(){  
+            userInput = "";
+            $(row).children().each(function(){  
+                userInput += $(this).val();
                 if ($(this).val() == targetWord[index]) {
                     $(this).addClass("correct");
                 } else if (targetWord.includes($(this).val())) {
@@ -67,36 +80,66 @@
                 } else {
                     $(this).addClass("wrong");
                 }
-                index++
+                index++;
+                
             });
-            // var userInput = document.getElementById("word-input").value.toLowerCase();
-            // var result = "";
-            // attempts++;
-            // document.getElementById("attempt-count").innerHTML = attempts;
 
-            // if (userInput.length !== 5) {
-            //     result = "Le mot doit contenir 5 lettres.";
-            // } else if (userInput === targetWord) {
-            //     result = "Félicitations ! Vous avez trouvé le mot.";
-            // } else {
-            //     for (var i = 0; i < userInput.length; i++) {
-            //         if (userInput[i] === targetWord[i]) {
-            //             result += "<span class='correct'>" + userInput[i] + "</span>";
-            //         } else if (targetWord.includes(userInput[i])) {
-            //             result += "<span class='exists'>" + userInput[i] + "</span>";
-                 
-            //         } else {
-            //             result += "<span class='wrong'>" + userInput[i] + "</span>";
-            //         }
-            //     }
-            // }
+            attempts++;
+            $("#attempt-count").innerHTML = 
+            console.log(userInput);
+            if (userInput === targetWord) {
+                Swal.fire({
+                    title: 'Félicitation! Vous avez gagné.',
+                    text: "Voulez-vous recommencer une partie?",
+                    icon: 'success',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Oui, Rejouer!',
+                    cancelButtonText: 'Non'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // axios.post('function.php?action=delete')
+                        // .then(function () {
+                        //     Swal.fire(
+                        //         'Supprimé!',
+                        //         'Le compte a bien été supprimé.',
+                        //         'success'
+                        //     ).then(function () {
+                        //         window.location.reload();
+                        //     });            
+                        // })
+                        resetGame();
+                    }
+                })
+            } else {
+                printWord(targetWord);
+            }
 
-            // document.getElementById("result").innerHTML = result;
+            console.log(targetWord);
         }
 
         function startGame() {
             $(".play-btn").fadeOut("slow");
+
+            var div = document.createElement("div");
+            div.setAttribute("id", "attempt-count");
+            $(".game").prepend(div); 
+            div.innerHTML = "tentatives: ";
+
+            var p = document.createElement("p");
+            $("#attempt-count").append(p); 
+
             chooseWord();
+        }
+
+        function resetGame() {
+            $('#box').children().remove();
+            $('#attempt-count').remove();
+
+            attempts = 0;
+
+            startGame();
         }
 
 
--- index.php
+++ index.php
@@ -27,7 +27,6 @@
             </div>
 
             <div class="game">
-                <div id="attempt-count">Tentatives: </div>
                 <div id="box"></div>
                 <button class="play-btn" onclick="startGame()">PLAY</button>
 
@@ -41,5 +40,6 @@
 
 </body>
 <script src="assets/js/game.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
 </html>
